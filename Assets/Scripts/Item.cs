bd4c33f18594b43e43d6ecc984098578b1ede2a4
Duane Byer <duane.byer@gmail.com>
2019-10-05 18:07:40 -0400

    Add items that can be picked up

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public bool pickedUp = false;
+
+    private Main main;
+    private SpriteRenderer sprite;
+    private float pickupAccel = 5.0f;
+    private float pickupVelZ;
+    private float pickupZ;
+    private State state;
+
+    private enum State {
+        ON_GROUND,
+        BEING_LIFTED,
+        PICKED_UP,
+        BEING_DROPPED
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.main = FindObjectOfType<Main>();
+        this.sprite = GetComponentInChildren<SpriteRenderer>();
+        this.state = State.ON_GROUND;
+        this.pickupVelZ = 0f;
+        this.pickupZ = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.state == State.BEING_LIFTED) {
+            // Calculate the parabola that takes this object right into the main characters hands.
+            float pickupVelZSq = 2f * this.pickupAccel * (this.main.height - this.pickupZ);
+            if (pickupVelZSq > 0f) {
+                this.pickupVelZ = Mathf.Sqrt(pickupVelZSq);
+                float deltaT = this.pickupVelZ / this.pickupAccel;
+                Vector2 displacement = this.transform.position - this.main.transform.position;
+                if (Mathf.Abs(deltaT) < 0.0001f) {
+                    this.state = State.PICKED_UP;
+                }
+                else {
+                    this.pickupZ += this.pickupVelZ * Time.deltaTime;
+                    this.transform.position -= (Vector3)displacement / deltaT * Time.deltaTime;
+                }
+            }
+            else {
+                this.state = State.PICKED_UP;
+            }
+        }
+        if (this.state == State.PICKED_UP) {
+            this.transform.position = this.main.transform.position;
+            this.pickupZ = this.main.height;
+        }
+        if (this.state == State.BEING_DROPPED) {
+            if (this.pickupZ > 0f) {
+                this.pickupVelZ -= this.pickupAccel * Time.deltaTime;
+            }
+            else if (this.pickupZ < 0f && this.pickupVelZ < 0f) {
+                this.pickupVelZ *= -0.5f;
+            }
+            this.pickupZ += this.pickupVelZ * Time.deltaTime;
+            if (Mathf.Abs(this.pickupZ) < 0.01f && Mathf.Abs(this.pickupVelZ) < 1f) {
+                this.state = State.ON_GROUND;
+            }
+        }
+        if (this.state == State.ON_GROUND) {
+            this.pickupZ = 0f;
+        }
+
+        Vector3 newSpritePos = this.sprite.transform.localPosition;
+        newSpritePos.y = this.pickupZ;
+        this.sprite.transform.localPosition = newSpritePos;
+    }
+
+    public void Pickup() {
+        this.pickedUp = true;
+        this.state = State.BEING_LIFTED;
+    }
+
+    public void Drop() {
+        this.pickedUp = false;
+        this.state = State.BEING_DROPPED;
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -6,8 +6,12 @@ public class Main : MonoBehaviour
 {
     private new Camera camera;
     private Vector2 waypoint;
+    private Item targetItem = null;
+    public float pickupRange = 0.5f;
+    public float walkRange = 0.05f;
     public float speed = 1;
-
+    public float height = 1.2f;
+    public Item item = null;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +23,50 @@ public class Main : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
+        // Drop items if commanded.
+        if (Input.GetAxis("Fire2") > 0f) {
+            if (this.item != null) {
+                this.item.Drop();
+                this.item = null;
+            }
+        }
+
+        // Look for any items being targeted by the left click.
+        if (Input.GetMouseButtonDown(0)) {
+            this.targetItem = null;
+            Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray);
+            foreach (RaycastHit2D hit in hits) {
+                Item hitItem = hit.transform.gameObject.GetComponent<Item>();
+                if (hitItem != null) {
+                    this.targetItem = hitItem;
+                    break;
+                }
+            }
+        }
+
+        // Otherwise, just set the target to the position clicked.
+        if (this.targetItem == null && Input.GetMouseButton(0)) {
             Vector3 mousePosition = this.camera.ScreenToWorldPoint(Input.mousePosition);
             this.waypoint = mousePosition;
         }
 
-
+        // Walk towards the target item (or point).
         Vector2 displacement = this.waypoint - (Vector2)this.transform.position;
-        if (displacement.magnitude > 0.01) {
+        if (this.targetItem != null) {
+            displacement = this.targetItem.transform.position - this.transform.position;
+        }
+        if (displacement.magnitude > this.walkRange) {
             Vector2 direction = displacement.normalized;
             direction *= Time.deltaTime * this.speed;
             this.transform.position += (Vector3)direction;
+        }
+        // If you don't already have an item, then pick it up.
+        if (this.item == null && this.targetItem != null && displacement.magnitude < this.pickupRange) {
+            this.item = this.targetItem;
+            this.item.Pickup();
+            this.waypoint = this.targetItem.transform.position;
+            this.targetItem = null;
         }
     }
 }
