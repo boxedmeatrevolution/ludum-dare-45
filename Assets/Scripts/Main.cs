747a5a03fa0df03c3040e0c431bece6e5caa7985
asemahle <aidan.mahler13@gmail.com>
2019-10-05 16:19:10 -0400

    Character moves!

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    private new Camera camera;
+    private Vector2 waypoint;
+    public float speed = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-		
+        this.camera = FindObjectOfType<Camera>();
+        this.waypoint = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = this.camera.ScreenToWorldPoint(Input.mousePosition);
+            this.waypoint = mousePosition;
+        }
+
+
+        Vector2 displacement = this.waypoint - (Vector2)this.transform.position;
+        if (displacement.magnitude > 0.01) {
+            Vector2 direction = displacement.normalized;
+            direction *= Time.deltaTime * this.speed;
+            this.transform.position += (Vector3)direction;
+        }
     }
 }
