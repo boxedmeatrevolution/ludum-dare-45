bf7ba019de6c7788babddd50e7076e38ee57d838
asemahle <aidan.mahler13@gmail.com>
2019-10-06 16:51:41 -0400

    Machine state working with clicks. Incomplete.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,6 +8,11 @@ public class Main : MonoBehaviour
     private new Camera camera;
     private Vector2 waypoint;
     private Item targetItem = null;
+
+    private Machine machine;
+    private OrbManager orbManager;
+    private bool isTargettingMachine = false;
+    
     public float pickupRange = 0.5f;
     public float walkRange = 0.05f;
     public float speed = 1;
@@ -20,6 +25,8 @@ public class Main : MonoBehaviour
         this.animator = GetComponentInChildren<Animator>();
         this.camera = FindObjectOfType<Camera>();
         this.waypoint = this.transform.position;
+        this.machine = GameObject.Find("Machine").GetComponent<Machine>();
+        this.orbManager = GameObject.Find("OrbManager").GetComponent<OrbManager>();
     }
 
     // Update is called once per frame
@@ -35,6 +42,7 @@ public class Main : MonoBehaviour
         // Look for any items being targeted by the left click.
         if (Input.GetMouseButtonDown(0)) {
             this.targetItem = null;
+            this.isTargettingMachine = false;
             Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray);
             foreach (RaycastHit2D hit in hits) {
@@ -46,10 +54,31 @@ public class Main : MonoBehaviour
             }
         }
 
-        // Otherwise, just set the target to the position clicked.
+        // Otherwise, just set the target to the position clicked... Or, go to the machine.
         if (this.targetItem == null && Input.GetMouseButton(0)) {
-            Vector3 mousePosition = this.camera.ScreenToWorldPoint(Input.mousePosition);
-            this.waypoint = mousePosition;
+            
+            // Check if player should go to machine
+            Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray);
+            foreach (RaycastHit2D hit in hits)
+            {
+                Machine machine = hit.transform.gameObject.GetComponentInParent<Machine>();
+                if (machine != null)
+                {
+                    this.isTargettingMachine = true;
+                    break;
+                }
+            }
+            if (this.isTargettingMachine)
+            {
+                // Go to machine
+                this.waypoint = this.machine.GetWaypoint().position;
+            }
+            else
+            {
+                // Go to mouse
+                this.waypoint = this.camera.ScreenToWorldPoint(Input.mousePosition);
+            }
         }
 
         // Walk towards the target item (or point).
@@ -57,6 +86,7 @@ public class Main : MonoBehaviour
         if (this.targetItem != null) {
             displacement = this.targetItem.transform.position - this.transform.position;
         }
+            
         if (displacement.magnitude > this.walkRange) {
             Vector2 direction = displacement.normalized;
             direction *= Time.deltaTime * this.speed;
@@ -77,6 +107,13 @@ public class Main : MonoBehaviour
             }
         } else {
             this.animator.Play("Main_Idle");
+
+            // If character has made it to the machine, then interact with the machine
+            if (this.isTargettingMachine)
+            {
+                this.machine.Interact();
+                this.isTargettingMachine = false;
+            }
         }
         // If you don't already have an item, then pick it up.
         if (this.item == null && this.targetItem != null && displacement.magnitude < this.pickupRange) {
---
+++ Assets/Scripts/OrbManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManager : MonoBehaviour
+{
+    private Orb[] orbs;
+    private bool orbIsPickedUp;
+    private Orb pickedUpOrb;
+
+    private List<Orb> orbsInMachine;
+    private List<Orb> orbsInMonster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.orbIsPickedUp = false;
+        this.orbs = GetComponentsInChildren<Orb>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // determine if any orb is picked up
+        this.orbIsPickedUp = false;
+        for (int i = 0; i < this.orbs.Length; i++)
+        {
+            Orb orb = this.orbs[i];
+            if (orb.item.pickedUp)
+            {
+                this.orbIsPickedUp = true;
+                this.pickedUpOrb = orb;
+                break;
+            }
+        }
+    }
+
+    public bool IsOrbPickedUp()
+    {
+        return this.orbIsPickedUp;
+    }
+
+    public Orb GetPickedUpOrb()
+    {
+        return this.pickedUpOrb;
+    }
+}
