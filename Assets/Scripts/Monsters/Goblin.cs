c488c3f2cea6d6df003182627e6c1a312341fd71
Duane Byer <duane.byer@gmail.com>
2019-10-06 15:58:16 -0400

    Add goblin

--- Assets/Scripts/Monsters/Goblin.cs
+++ Assets/Scripts/Monsters/Goblin.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AngryMonster : Monster
-{
+public class Goblin : Monster {
     // Start is called before the first frame update
-    protected override void Start()
-    {
+    protected override void Start() {
         base.Start();
     }
 
     // Update is called once per frame
-    protected override void Update()
-    {
+    protected override void Update() {
         base.Update();
     }
 
     // Choose whether to threaten, panic, or just keep wandering when another monster wanders into range.
     protected override State ChooseThreatenOffensive(Monster other) {
-        return State.THREATEN;
+        return State.PANIC;
     }
 
     // Choose whether to threaten, panic, or just keep wandering when another monster threatens.
     protected override State ChooseThreatenDefensive(Monster other) {
-        return State.THREATEN;
+        return State.PANIC;
     }
 
     // Choose whether you survive the fight with the other monster.
     protected override bool SurviveFight(Monster other) {
-        return false;
+        if (other is FireSalamander) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
